510ad273b98645da5f6df4d0a80624e4ffde7168
yunus kupucu <yunus.kupucu@gmail.com>
2025-07-25 10:31:23 +0300

    feat: add savedWordsSlice for managing saved words in Redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import levelReducer from './levelSlice';
+import savedWordsReducer from './savedWordsSlice';
 export const store = configureStore({
-  reducer: { level: levelReducer },
+  reducer: { level: levelReducer, savedWords: savedWordsReducer },
 });
