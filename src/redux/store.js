51d8d81cbcb411c453ec0597fb283c56648a0120
yunus kupucu <yunus.kupucu@gmail.com>
2025-07-26 19:09:41 +0300

    Remove Vite configuration file as it is no longer needed for the project setup.

--- src/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import levelReducer from './levelSlice';
-import savedWordsReducer from './savedWordsSlice';
-export const store = configureStore({
-  reducer: { level: levelReducer, savedWords: savedWordsReducer },
-});
