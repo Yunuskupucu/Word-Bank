0cd5c1b6d186b929feed4ee93c683b9cb44ba42f
yunus kupucu <yunus.kupucu@gmail.com>
2025-07-29 14:46:41 +0300

    refactor: clean up index.js by removing comments and simplifying error handling

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,12 @@ import authRoutes from './routes/authRoute.js';
 import userRoutes from './routes/userRoutes.js';
 import { connectDB } from './connect.js';
 
-// .env dosyasını yükle
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-// CORS ayarları
-app.use(express.json()); // Body parse middleware
+app.use(express.json());
 app.use(
   cors({
     origin: 'http://localhost:5173',
@@ -21,26 +19,20 @@ app.use(
   })
 );
 
-// Middleware
-
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 
-// Error handling middleware
-app.use((err, req, res, next) => {
+app.use((err, res) => {
   console.error(err.stack);
   res
     .status(500)
     .json({ message: 'Bir şeyler ters gitti!', error: err.message });
 });
 
-// Test route
-app.get('/', (req, res) => {
+app.get('/', (res) => {
   res.json({ message: 'WordBank Backend API çalışıyor!' });
 });
 
-// Server'ı başlat
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
